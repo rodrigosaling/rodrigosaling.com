30524a0e2e1ee2c55cd8bdffcc73e454a8463740
Rodrigo Saling <rodrigosaling@gmail.com>
2021-10-23 19:16:18 -0300

    adicionado sitemap no config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     'gatsby-plugin-postcss',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
