7bfac7091c070c524fd1445771785033e74a0aa1
Rodrigo Saling <rodrigosaling@gmail.com>
2022-01-15 19:55:22 -0300

    atualiza o siteUrl no gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.rodrigosaling.com',
+    siteUrl: 'https://www.rodrigosaling.com/',
     title: 'Rodrigo Saling',
     author: 'Rodrigo Saling',
   },
