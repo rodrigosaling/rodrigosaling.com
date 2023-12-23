a8c013859649830fc2c829f835ed12bab0c104f3
Rodrigo Saling <rodrigosaling@gmail.com>
2023-12-23 12:22:42 -0300

    adds favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
-// import { Link } from 'gatsby';
+import { Link } from 'gatsby';
 import '../styles/index.css';
+
+import Icon from '../images/upside-down-all-paperclip-logo.svg';
 
 const IndexPage: React.FC<PageProps> = () => (
   <div className="mx-auto max-w-3xl px-4">
     <header className="py-3 border-b-neutral-300 border-b mt-5 flex justify-between">
       <h1 className="font-bold">Rodrigo Saling</h1>
+      {/* <Link to="/about-me#lwhats-up-with-the-logo" className=""> */}
+      <img
+        alt="Website logo, a triangle paperclip that resembles an upside down A"
+        src={Icon}
+        width={20}
+      />
+      {/* </Link> */}
 
       {/* <nav>
         <ul className="flex gap-6">
@@ -18,6 +27,11 @@ const IndexPage: React.FC<PageProps> = () => (
           <li>
             <Link to="/projects" className="underline hover:no-underline">
               Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog" className="underline hover:no-underline">
+              Blog
             </Link>
           </li>
         </ul>
@@ -97,5 +111,6 @@ export const Head: HeadFC = () => (
       name="description"
       content="Rodrigo is a Software Engineer located in Porto Alegre, working remotely for League Inc."
     />
+    <link rel="icon" type="image/svg" href={Icon}></link>
   </>
 );
