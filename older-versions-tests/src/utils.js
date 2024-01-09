a9f162b09b442ba36bc46cc05f8615df69268025
Rodrigo Saling <rodrigosaling@gmail.com>
2024-01-09 11:48:05 -0300

    removes old gatsby test

--- older-versions-tests/src/utils.js
+++
@@ -1,7 +0,0 @@
-export const publishedAt = (date) =>
-  new Intl.DateTimeFormat('pt-BR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    timeZone: 'America/Sao_Paulo',
-  }).format(new Date(date));
