9b90f30411ecd383eb2cca32ff00556f589b91ec
Rodrigo Saling <rodrigosaling@gmail.com>
2024-01-19 11:48:11 -0300

    adds Gerador de Senhas as one of my projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,25 +1,41 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
-import { Link } from 'gatsby';
 import Template from '../templates/template';
 import { HeadingOne } from '../components/heading-one';
+import { HeadingTwo } from '../components/heading-two';
 
 const ProjectsPage: React.FC<PageProps> = () => {
   return (
     <Template>
-      <>
-        <HeadingOne>Projects</HeadingOne>
-        <p>
-          You can check{' '}
-          <a
-            href="https://github.com/rodrigosaling"
-            className="underline hover:no-underline hover:text-orange-400"
-          >
-            my GitHub profile
-          </a>{' '}
-          in the meantime.
-        </p>
-      </>
+      <HeadingOne>Projects</HeadingOne>
+
+      <HeadingTwo>Gerador de Senhas (Password Generator)</HeadingTwo>
+
+      <p>
+        An easy-to-remember password generator that uses Brazilian Portuguese
+        words. Still a working in progress. Uses SvelteKit.
+      </p>
+      <p>
+        You can{' '}
+        <a href="https://gerador-senhas.rodrigosaling.com/">check it online</a>{' '}
+        or go directly to{' '}
+        <a href="https://github.com/rodrigosaling/gerador-senhas">
+          the repository
+        </a>
+        .
+      </p>
+
+      <HeadingTwo>Other Projects</HeadingTwo>
+      <p>
+        You can check{' '}
+        <a
+          href="https://github.com/rodrigosaling"
+          className="underline hover:no-underline hover:text-orange-400"
+        >
+          my GitHub profile
+        </a>{' '}
+        in the meantime.
+      </p>
     </Template>
   );
 };
